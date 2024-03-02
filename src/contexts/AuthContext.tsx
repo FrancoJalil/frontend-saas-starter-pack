6ed4f0986fd2fbc8dd99eea22f52fb9db01304e4
Franco Jalil <jalilfranco9@gmail.com>
2024-03-01 23:22:57 -0300

    refresh token and protected-route test

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,12 +1,13 @@
 import { createContext } from 'react'
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from "react-router-dom";
 import { FormErrors } from '../pages/Login/Login'
 
 type AuthContextType = {
   user: any
+  authTokens: any
   loginUser: any
   logoutUser: any
 };
@@ -26,6 +27,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const navigate = useNavigate()
   let [authTokens, setAuthTokens] = useState(() => tokkk ? JSON.parse(tokkk) : null)
   let [user, setUser] = useState(() => tokkk ? jwtDecode(tokkk) : null)
+  let [loadingWebsite, setIsLoadingWebsite] = useState(true)
 
   let loginUser = async (e: React.FormEvent<HTMLFormElement>, email: string, password: string, setIsLoading: any, setErrors: any, errors: any) => {
     e.preventDefault()
@@ -43,7 +45,6 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
       })
 
       const data = await response.json()
-
 
       if (response.status === 200) {
         setAuthTokens(data)
@@ -71,15 +72,65 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     navigate('/login')
   }
 
+  let updateToken = async () => {
+    console.log("UPDATE TOKEN CALLED")
+    try {
+      let response = await fetch('http://localhost:8000/user/token/refresh/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ refresh: authTokens?.refresh })
+      })
+
+      const data = await response.json()
+
+
+      if (response.status === 200) {
+        setAuthTokens(data)
+        setUser(jwtDecode(data.access))
+        localStorage.setItem('authTokens', JSON.stringify(data))
+
+
+      } else {
+        throw new Error('Invalid token');
+      }
+    } catch (err) {
+      console.error(err)
+      logoutUser()
+    }
+
+    if (loadingWebsite) {
+      setIsLoadingWebsite(false)
+    }
+  }
+
   let contextData = {
     user: user,
+    authTokens: authTokens,
     loginUser: loginUser,
     logoutUser: logoutUser
   }
 
+  useEffect(() => {
+
+    if (loadingWebsite) {
+      updateToken()
+    }
+
+    let fourMinutes = 1000 * 60 * 4
+    let interval = setInterval(() => {
+      if (authTokens) {
+        updateToken()
+      }
+    }, fourMinutes)
+    return () => clearInterval(interval)
+
+  }, [authTokens, loadingWebsite])
+
   return (
     <AuthContext.Provider value={contextData} >
-      {children}
+      { loadingWebsite ? null : children}
     </AuthContext.Provider>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,53 @@
 import { AuthContext } from '../../contexts/AuthContext'
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Navigate } from "react-router-dom"
 
 type Props = {}
 
+type ResponseType = {
+    email: any
+    tokens: any
+}
+
 export const Home = () => {
 
-    let { user, logoutUser } = useContext(AuthContext)
+    let { user, logoutUser, authTokens } = useContext(AuthContext)
+
+    const [info, setInfo] = useState<ResponseType>()
+
+    useEffect(() => {
+        getInfo()
+        console.log(info)
+    }, [])
+
+    const getInfo = async () => {
+        try {
+            let response = await fetch('http://localhost:8000/user/protected-view/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + String(authTokens.access)
+                }
+            })
+
+            const data = await response.json()
+            console.log(data)
+
+            if (response.status === 200) {
+                setInfo(data)
+                console.log(info)
+
+            } else if (response.status === 401) {
+                logoutUser()
+            } else {
+                throw new Error('Invalid response');
+            }
+        } catch (err) {
+            console.error(err)
+        } finally {
+        }
+    }
 
     return (
         <>
@@ -22,9 +62,12 @@ export const Home = () => {
             }
 
 
-            {user &&
-                <p>Hello {user.email}</p>
-            }
+            <>
+                <p>Hello {info?.email}!</p>
+                <p>Your tokens: {info?.tokens}!</p>
+            </>
+
+
         </>
 
     )
